308db04c01a6b254dbde7a73066f0da0261a7fdd
Luke Schields <luke.j.schields@gmail.com>
2024-01-12 15:45:51 -0500

    added text explanation box for heat charts and download functionality for images

--- main.js
+++ main.js
@@ -2,3 +2,29 @@
 function switchVis(chartId) {
   document.querySelector('iframe').src = 'https://datawrapper.dwcdn.net/' + chartId;
 }
+
+function shareOnTwitter() {
+  var imageElement = document.getElementById('test-wordcloud.png');
+
+  if (!imageElement) {
+    console.error('Image element not found.');
+    return;
+    }
+
+  var imageSource = imageElement.src;
+
+  if (!imageSource) {
+    console.error('Image source not found.');
+    return;
+    }
+
+  var encodedImageSource = encodeURIComponent(imageSource);
+  var twitterShareUrl = 'https://twitter.com/intent/tweet?text=Check%20out%20this%20image&url=' + encodedImageSource;
+
+  // Open Twitter share window
+  var twitterWindow = window.open(twitterShareUrl, '_blank');
+
+  if (!twitterWindow) {
+    console.error('Failed to open Twitter share window. Make sure popups are allowed for this site.');
+    }
+}
